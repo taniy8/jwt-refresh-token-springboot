5b465a78f6b3b7554cf03ce0135528038d0f22d4
Taniya <taniyasaxena804@gmail.com>
2026-03-26 22:40:37 +0530

    Add Annotation for response

--- src/main/java/com/example/auth/dto/AuthResponse.java
+++ src/main/java/com/example/auth/dto/AuthResponse.java
@@ -1,6 +1,13 @@
 package com.example.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
     private String accessToken;
     private String refreshToken;
+
+
 }
